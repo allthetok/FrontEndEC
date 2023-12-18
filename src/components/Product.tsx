60662a78be2c2d5dd735542de584284482315fed
allthetok <al.tokjuman@gmail.com>
2023-12-18 18:44:57 -0500

    new npm command and using mockdata

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ultraBoost } from '../helpers/mockdata'
 
 const Product = () => {
 	return (
-		<div className='text-3l flex font-bold underline' >
-			Hello
+		<div className='flex font-bold underline text-2xl' >
+			<p className='flex text-center text-sm'>
+				{ultraBoost.filter((el: any) => el.images.length !== 0)[0].description}
+			</p>
 		</div>
 	)
 }
---
+++ src/helpers/mockdata.js
@@ -0,0 +1,348 @@
+const ultraBoost = [
+	{
+		id: 1,
+		brand: 'Adidas',
+		modelName: 'UltraBoost',
+		name: 'UltraBoost Light Shoes',
+		releaseDate: '2023-01-28',
+		colors: [
+			'Cloud White/Cloud White/Crystal White',
+			'Dark Blue/Cream White/Core Black'
+		],
+		images: ['https://assets.adidas.com/images/w_600,f_auto,q_auto/c608f554cb3b4d12b392af000188c513_9366/Chaussure_Ultraboost_1.0_noir_HQ4199_01_standard.jpg', 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/333d70cd5438497e8171af4501038746_9366/Chaussure_Ultraboost_1.0_noir_HQ4199_010_hover_standard.jpg', 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/bf7846b566e24a318217af000188a4d4_9366/Chaussure_Ultraboost_1.0_noir_HQ4199_012_hover_standard.jpg'],
+		price: '260',
+		description: `Experience epic energy with the new Ultraboost Light, our lightest Ultraboost ever. 
+	The magic lies in the Light BOOST midsole, a new generation of adidas BOOST. Its unique molecule design achieves the lightest BOOST foam to date. 
+	With hundreds of BOOST capsules bursting with energy and ultimate cushioning and comfort, some feet really can have it all.`
+	},
+	{
+		id: 2,
+		brand: 'Adidas',
+		modelName: 'UltraBoost',
+		name: 'UltraBoost 1.0 Shoes',
+		releaseDate: '2019-05-10',
+		colors: [
+			'Core Black / Core Black / Beam Green F22',
+			'Grey Three / Grey Five / Core Black'
+		],
+		images: [],
+		price: '260',
+		description: `From a walk in the park to a weekend run with friends, these adidas Ultraboost 1.0 shoes are designed to keep you comfortable. 
+		An adidas PRIMEKNIT upper gently hugs your feet while BOOST on the midsole cushions from the first step to the last mile. 
+		The Stretchweb outsole flexes naturally for an energized ride, and Continental™ Rubber gives you the traction you need to keep that pep in your step.`
+	},
+	{
+		id: 3,
+		brand: 'Adidas',
+		modelName: 'UltraBoost',
+		name: 'UltraBoost 22 Shoes',
+		releaseDate: '2021-12-06',
+		colors: [
+			'Core Black / Core Black / Core Black',
+			'Core Black / Core Black / Lucid Blue S23'
+		],
+		images: [],
+		price: '175',
+		description: `Even on your busiest days, running is a priority. These adidas trainers make every mile count, 
+		thanks to the incredible energy return of BOOST cushioning. The Linear Energy Push system increases stability 
+		for a smooth and steady stride, and the Stretchweb outsole delivers natural flex to keep you moving comfortably, no matter your distance or pace.`
+	},
+	{
+		id: 4,
+		brand: 'Adidas',
+		modelName: 'UltraBoost',
+		name: 'UltraBoost 4.0 Shoes',
+		releaseDate: '2021-03-04',
+		colors: [
+			'Core Black / Core Black / Active Red',
+			'Cloud White / Cloud White / Core Black'
+		],
+		images: [],
+		price: '200',
+		description: `A young legend. The adidas Ultraboost debuted in 2015, and it became a go-to far beyond the realm of running. 
+		These shoes have a soft knit upper that offers ventilation where you need it most. 
+		They ride on our original tooling, with a Boost midsole for comfort you need to feel to believe`
+	}
+]
+
+const superStar = [
+	{
+		id: 5,
+		brand: 'Adidas',
+		modelName: 'Superstar',
+		name: 'Superstar Shoes',
+		releaseDate: '2019-11-01',
+		colors: [
+			'Cloud White / Core Black / Cloud White',
+			'Core Black / Cloud White / Core Black',
+			'Cloud White / Collegiate Green / Cloud White',
+		],
+		images: [],
+		price: '130',
+		description: `Built for basketball, adopted by hip hop and skate, the classic leather Superstar changed the game the moment it stepped off the court. 
+		The serrated 3-Stripes mark, iconic shell style toe, and box logo makes this one of the true Originals.`
+	},
+	{
+		id: 6,
+		brand: 'Adidas',
+		modelName: 'Superstar',
+		name: 'Superstar Gore-Tex Shoes',
+		releaseDate: '2023-11-02',
+		colors: [
+			'Core Black / Core Black / Cloud White',
+		],
+		images: [],
+		price: '180',
+		description: `Whether the day brings rain or snow, these adidas Superstar sneakers have you covered. A GORE-TEX membrane provides breathable protection 
+		from the elements without sacrificing the classic style these shoes are known for. First hitting the court in 1970, the Superstar has spent decades exploring
+		street culture. A fresh nubuck upper keeps this iconic shell-toe shoe ready for modern times. Waterproof and windproof, they'll keep your feet comfortable and
+		your stride going strong no matter what's on the agenda outside.`
+	},
+]
+
+const stanSmith = [
+	{
+		id: 7,
+		brand: 'Adidas',
+		modelName: 'Stan Smith',
+		name: 'Bape 30th Anniversary Black',
+		releaseDate: '2023-11-18',
+		colors: [
+			'CORE BLACK/CORE BLACK/GOLD METALLIC',
+		],
+		images: [],
+		price: '160',
+		description: `The YEEZY BOOST 350 V2 features an upper composed of re-engineered Primeknit. The post-dyed monofilament side stripe is woven into the upper. 
+		Reflective threads are woven into the laces. The midsole utilizes adidas’ innovative BOOST™ technology.`
+	}
+]
+
+const yeezy350 = [
+	{
+		id: 8,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Boost 350',
+		name: 'Yeezy Boost 350 V2',
+		releaseDate: '2022-03-21',
+		colors: [
+			'Bone/Bone/Bone',
+			'Onyx/Onyx/Onyx',
+			'White/Core Black/Red',
+			'Beluga Reflective/Steeple Gray/Solar Red'
+
+		],
+		images: [],
+		price: '230',
+		description: `The YEEZY BOOST 350 V2 is the lifestyle runner from adidas and Kanye West. It features an upper composed of re-engineered Primeknit. The post-dyed 
+		monofilament side stripe is woven into the upper. Reflective threads are woven into the laces. The midsole utilizes adidas’ innovative BOOST™ technology.`
+	}
+]
+
+const yeezy500 = [
+	{
+		id: 9,
+		brand: 'Yeezy',
+		modelName: 'Yeezy 500',
+		name: 'Yeezy Boost 500',
+		releaseDate: '2018-07-07',
+		colors: [
+			'Utility Black/Utility Black/Utility Black',
+			'Bone White/Bone White/Bone White',
+		],
+		images: [],
+		price: '200',
+		description: `In July 2018, Kanye West and adidas released the Yeezy 500. The retro-inspired silhouette counterbalances a sleek design with its bulky form. 
+		What distinguishes this colorway from the standard triple-black finish is a unique use of tone – it's a semi-faded black that coats the combination of mesh 
+		and upper suede, as opposed to the pitch-black variety more common in the firmament of the sneaker.`
+	},
+	{
+		id: 10,
+		brand: 'Yeezy',
+		modelName: 'Yeezy 500',
+		name: 'Yeezy Boost 500 High Boot',
+		releaseDate: '2023-08-14',
+		colors: [
+			'Utility Black/Utility Black/Utility Black'
+		],
+		images: [],
+		price: '300',
+		description: `The adidas Yeezy 500 Tactical Boot 'Utility Black' showcases a black ballistic mesh upper with matching suede overlays, and is fastened with speed 
+		lacing for quick adjustments. The rubber mudguard seamlessly integrates with the podular adiPRENE+ midsole, which is borrowed from the original Yeezy 500 design. 
+		The boot’s extended padded collar reaches up to the calf for optimal fit.`
+	},
+	{
+		id: 11,
+		brand: 'Yeezy',
+		modelName: 'Yeezy 500',
+		name: 'Yeezy Boost 500 High',
+		releaseDate: '2019-12-16',
+		colors: [
+			'Slate/Slate/Slate'
+		],
+		images: [],
+		price: '220',
+		description: `Celebrity Kanye West retools the 500 profile with the adidas Yeezy 500 High. Debuted in December 2019, this revamp boasts a Slate-colored neoprene base 
+		layered with suede and tumbled leather elements. Utility laces secure the tongue and a patterned mudguard bolsters the sidewall. The molded midsole is supported by 
+		Adiprene cushioning while the robust rubber outsole utilizes a wavy tread pattern.`
+	},
+]
+
+const yeezy750 = [
+	{
+		id: 12,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Boost 700',
+		name: 'Yeezy Boost 700 V2 Tephra',
+		releaseDate: '2019-06-15',
+		colors: [
+			'Tephra/Tephra/Tephra'
+		],
+		images: [],
+		price: '300',
+		description: `The Yeezy Boost 700 V2 'Tephra' brings a subdued colorway to the second iteration of the Yeezy 700 line. The shoe's upper is built with a layered mix of mesh and suede, 
+		finished in neutral hues. A darker look emerges on the signature bulky sole unit, which houses a full-length Boost midsole for cushioning. Underfoot, a gum rubber outsole provides 
+		traction while offering an elevated finish.`
+	},
+	{
+		id: 13,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Boost 700',
+		name: 'Yeezy 700 V3 Fade Carbon',
+		releaseDate: '2022-05-21',
+		colors: [
+			'Fade Carbon/Fade Carbon-Fade Carbon'
+		],
+		images: [],
+		price: '210',
+		description: `Distinguished by a fade gradient that moves from a mauve hue to a bright magenta at the heel, the adidas Yeezy 700 V3 ‘Fade Carbon’ is a lifestyle runner in an eyecatching colorway.
+		A translucent RPU cage overlays the engineered mesh upper. An interior bootie offers a snug, comfortable fit while traction and grip are assured from the herringbone-patterned rubber outsole.`
+	},
+	{
+		id: 14,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Boost 700',
+		name: 'Yeezy Boost 700 Hi-Res',
+		releaseDate: '2022-08-02',
+		colors: [
+			'Hi-Res Blue/Hi-Res Blue-Hi-Res Blue',
+			'Hi-Res Red/Hi-Res Red/Hi-Res Red'
+		],
+		images: [],
+		price: '300',
+		description: `The adidas Yeezy Boost 700 'Hi-Res Blue'/'Hi-Res Red' features a vibrant shade across most of the lifestyle runner designed by Kanye West. Hi-Res Blue/Red appears on parts of the upper and midsole, 
+		contrasted by black suede overlays and grey near the front of the shoe and toe cap. The sculpted midsole features a hit of red near the front, while full-length Boost cushioning is embedded right 
+		above the gum rubber outsole.`,
+	},
+]
+
+const yeezyOpen = [
+	{
+		id: 15,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Foam Runner | Yeezy Slides',
+		name: 'Yeezy Slides',
+		releaseDate: '2022-03-07',
+		colors: [
+			'Onyx/Onyx/Onyx',
+			'Granite/Granite/Granite',
+			'Bone/Bone/Bone'
+		],
+		images: [],
+		price: '60',
+		description: `As part of Yeezy’s sixth collection, Kanye West and adidas introduced the Yeezy Slide in 2018. Featuring monotone colorways, the Yeezy Slide is an elevated pool slide design, featuring injection-molded 
+		construction, a shark-tooth outsole, and a forefoot strap. The Yeezy Slides maintain the look and feel of a classic sandal, but with a few design touches to set them apart. Key colorways of the Yeezy Slides include 
+		“Bone,” “Sand” and “Azure.”`
+	},
+	{
+		id: 16,
+		brand: 'Yeezy',
+		modelName: 'Yeezy Foam Runner | Yeezy Slides',
+		name: 'Yeezy Foam Runner',
+		releaseDate: '2022-04-16',
+		colors: [
+			'Sulfur/Sulfur/Sulfur',
+			'Stone Sage/Stone Sage/Stone Sage',
+			'Vermillion/Vermillion/Vermillion'
+		],
+		images: [],
+		price: '90',
+		description: `The Yeezy Foam Runner features a unique look, utilizing a one-piece foam upper with a porous design, crafted from environmentally friendly algae. Kanye West and adidas unleashed a lifestyle slip-on with
+		no shoelaces, arriving in many different colorways. Explore a selection of Foam RNRs including 'Carbon,' 'Stone Salt,' 'Stone Taupe' and 'Desert Sand.'`
+	},
+]
+
+const nikeAF1 = [
+	{
+		id: 17,
+		brand: 'Nike',
+		modelName: 'Nike Air Force 1',
+		name: 'Nike Air Force 1 07',
+		releaseDate: '2020-12-09',
+		colors: [
+			'White/White/White',
+			'Black/Black/Black',
+		],
+		images: [],
+		price: '90',
+		description: `Air Force 1s have come a long way since their debut as the first on-court shoe to feature Nike Air technology. But despite this, the cultural icon stays true to its roots with the same soft and springy 
+		cushioning that changed sneaker history. This sneaker-head favourite delivers lasting comfort, while its padded ankle and tongue make for a soft ride. The rubber sole features the iconic circular pattern for durable 
+		traction. Originally designed for performance hoops, Nike Air cushioning adds lightweight, all-day comfort.`
+	},
+	{
+		id: 18,
+		brand: 'Nike',
+		modelName: 'Nike Air Force 1',
+		name: 'Nike Air Force 1 07 LV8',
+		releaseDate: '2023-01-03',
+		colors: [
+			'White/Stadium Green/Pale Ivory/University Red',
+			'Tour Yellow/Sail-Black-Tour Yellow'
+		],
+		images: [],
+		price: '130',
+		description: `Never mistaken, the Nike Air Force 1 LV8 is the hardwood classic that redefined basketball shoes and sneaker culture forever. Suede details atop synthetic leather on the upper deliver the iconic style 
+		that landed it at the pinnacle of street, sport and culture for decades.`
+	},
+]
+
+const nikeAirM97 = [
+	{
+		id: 19,
+		brand: 'Nike',
+		modelName: 'Nike Air Max 97',
+		name: 'Nike Air Max 97',
+		releaseDate: '2018-12-14',
+		colors: [
+			'White/White/White',
+			'Black/Black/Black',
+			'Metallic Gold/Varsity Red/White-Black'
+		],
+		images: [],
+		price: '160',
+		description: `Inspired by nature, the Air Max 97 ushered in a new era in footwear for a modern world. The silhouette was introduced with a full-length Air unit all around the midsole for maximum comfort for both 
+		runners and everyday wear. Like the 95, it maintained its layered look, but the 3M detail was what sealed the deal. Browse our selection of AM97, including the latest releases.`
+	},
+]
+
+const nikeDunk = [
+	{
+		id: 20,
+		brand: 'Nike',
+		modelName: 'Nike Air Max 97',
+		name: 'Nike Air Max 97',
+		releaseDate: '2018-12-14',
+		colors: [
+			'White/White/White',
+			'Black/Black/Black',
+			'Metallic Gold/Varsity Red/White-Black'
+		],
+		images: [],
+		price: '160',
+		description: `Inspired by nature, the Air Max 97 ushered in a new era in footwear for a modern world. The silhouette was introduced with a full-length Air unit all around the midsole for maximum comfort for both 
+		runners and everyday wear. Like the 95, it maintained its layered look, but the 3M detail was what sealed the deal. Browse our selection of AM97, including the latest releases.`
+	},
+]
+
+
+
+export { ultraBoost, superStar, stanSmith, yeezy350, yeezy500, yeezy750, yeezyOpen, nikeAF1, nikeAirM97, nikeDunk }
