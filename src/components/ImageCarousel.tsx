3ead681e95038dd1a6c5cb69ca313eddc9ad85b5
allthetok <al.tokjuman@gmail.com>
2024-01-09 20:10:02 -0500

    updated images carousel to use query params to show proper color images

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -10,16 +10,16 @@ type ImageProps = {
 }
 
 const ImageCarousel = ({ colors, colorQuery }: ImageProps ) => {
-	console.log(colorQuery)
+	const colorsFiltered: Colors[] = colors.filter((indColor: Colors) => indColor.color === colorQuery)
 	return (
 		<div className='lg:w-1/2 w-full lg:h-auto h-64'>
 			<CarouselProvider
 				naturalSlideWidth={496}
 				naturalSlideHeight={496}
-				totalSlides={colors[0].images.length}
+				totalSlides={colorsFiltered[0].images.length}
 				infinite>
 				<Slider>
-					{colors[0].images.map((image: string, index: number) => (
+					{colorsFiltered[0].images.map((image: string, index: number) => (
 						<Slide index={index} key={index}>
 							<Image
 								src={image}
@@ -30,7 +30,7 @@ const ImageCarousel = ({ colors, colorQuery }: ImageProps ) => {
 					))}
 				</Slider>
 				<div className='flex justify-center gap-4 py-4'>
-					{colors[0].images.map((image: string, index: number) => (
+					{colorsFiltered[0].images.map((image: string, index: number) => (
 						<Dot key={index} slide={index} className='h-20 w-20 disabled:opacity-50 rounded-lg overflow-hidden'>
 							<Image className='object-cover h-full w-full' src={image} alt={`Image ${index}`} hasMasterSpinner={false} />
 						</Dot>
