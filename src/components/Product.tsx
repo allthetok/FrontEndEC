6dd021fcc1be4ccb81f2ce653ebb9d53c2e4dba7
allthetok <al.tokjuman@gmail.com>
2024-01-18 21:28:07 -0500

    finished fixing hydration error for link, added more links instead of wrapping entire component in link

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -11,17 +11,18 @@ type ProductProps = {
 
 const Product = ({ product }: ProductProps) => {
 	return (
-		// <Link className='group text-sm' href={from === 'product' ? `/product/${product.name}` : `product/${product.name}`}>
 		<div className='group text-sm'>
 			<div className='w-full overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-100 group-hover:opacity-75'>
-				<img className='h-full w-full object-cover object-center product-image' src={product.colors[0].images[0]} alt={`${product.name}`}/>
-				{/* <img className='h-full w-full object-fill object-center product-image' src={product.colors[0].images[0]} alt={`${product.name}`}/> */}
-
+				<Link href={`/product/${product.name}`}>
+					<img className='h-full w-full object-cover object-center product-image' src={product.colors[0].images[0]} alt={`${product.name}`}/>
+				</Link>
 			</div>
 			<h3 className='mt-4 font-medium flex flex-1 items-center justify-between'>
-				<div className='hover:underline'>{product.name}</div>
+				<Link href={`/product/${product.name}`}>
+					<div className='hover:underline'>{product.name}</div>
+				</Link>
 				<Link href={`/products/?brand=${product.brand}`}>
-					<span className='pt-1 text-xs text-gray-500 uppercase'>{product.brand}</span>
+					<span className='pt-1 text-xs text-gray-500 uppercase hover:underline'>{product.brand}</span>
 				</Link>
 			</h3>
 			<p className='mt-2 font-medium text-sm'>
@@ -31,7 +32,7 @@ const Product = ({ product }: ProductProps) => {
 	)
 }
 
-// <Link className='group text-sm' href={`/product/${product.name}`}>
+// 		<Link className='group text-sm' href={`/product/${product.name}`}>
 // 			<div className='w-full overflow-hidden rounded-lg border-2 border-gray-200 bg-gray-100 group-hover:opacity-75'>
 // 				<img className='h-full w-full object-cover object-center product-image' src={product.colors[0].images[0]} alt={`${product.name}`}/>
 // 				{/* <img className='h-full w-full object-fill object-center product-image' src={product.colors[0].images[0]} alt={`${product.name}`}/> */}
