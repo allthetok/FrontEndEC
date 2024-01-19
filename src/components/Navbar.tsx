3868bdea361682428a95d8d886a39d4bae469095
allthetok <al.tokjuman@gmail.com>
2024-01-18 22:18:23 -0500

    searchbar flex row items styling complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 'use client'
+import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import React from 'react'
 import { IconButton } from '@mui/material'
-import { Font50Sx } from '@/sx/styling'
 import { Search } from './Search'
+import { Font50Sx } from '@/sx/styling'
+
 
 const Navbar = () => {
 	return (
@@ -15,10 +16,12 @@ const Navbar = () => {
 					<Link className='flex items-center mb-4 md:mb-0' href='/'>
 						<Image src='/AT Kicks-logos_transparent.png' alt='AT Kicks Logo' width={150} height={150} className='scale-150'/>
 					</Link>
-					<Search />
-					<h1 className='font-bold text-6xl'>
-						AT KICKS
-					</h1>
+					<div className='flex flex-row gap-16'>
+						<Search />
+						<h1 className='font-bold text-6xl'>
+							AT KICKS
+						</h1>
+					</div>
 					<IconButton onClick={() => console.log('clicked cart')}>
 						<ShoppingCartIcon sx={Font50Sx} htmlColor='black'/>
 					</IconButton>
@@ -27,5 +30,11 @@ const Navbar = () => {
 		</header>
 	)
 }
+
+{/* <Search />
+	<h1 className='font-bold text-6xl ml-[-1000px]'>
+		AT KICKS
+	</h1> 
+*/}
 
 export { Navbar }
