82dc62068f607ae25e4e1d398c9b37da850973b4
allthetok <al.tokjuman@gmail.com>
2024-02-04 14:30:27 -0500

    more toast notification styling

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 import React, { useEffect, useRef, useState } from 'react'
 import { useRouter } from 'next/navigation'
@@ -5,12 +6,10 @@ import { useShoppingCart } from 'use-shopping-cart'
 import { ColorSizes, Colors, ProductObj, ProductSizes } from '@/helpers/types/fetypes'
 import { prodToStripeProd } from '@/helpers/fctns'
 import { Button } from '@mui/material'
-import * as Toast from '@radix-ui/react-toast'
+import { ToastProvider, Root as ToastRoot, Title, Description, Viewport } from '@radix-ui/react-toast'
 import { SizeButton } from './SizeButton'
 import { ActiveSizeButtonSx, AddToCartSx, AdditionalCartSx, SizeButtonSx } from '@/sx/styling'
-
 import './FullProduct.css'
-
 
 type OptionsProps = {
 	colorQuery: string | string[],
@@ -88,7 +87,7 @@ const Options = ({ colorQuery, productDtl }: OptionsProps) => {
 					</p>
 				</div> */}
 
-				<Toast.Provider swipeDirection='right'>
+				<ToastProvider swipeDirection='right'>
 					<div className='mx-auto flex flex-col items-center'>
 						<Button disabled={sizeOption === '' || (sizeOption !== '' ? sizesFiltered[0].sizes.filter((shoeSize: ProductSizes) => shoeSize.size === sizeOption)[0].amount === 0 : true)} sx={AdditionalCartSx} onClick={handleAdd}>
 							Add to Cart
@@ -97,26 +96,25 @@ const Options = ({ colorQuery, productDtl }: OptionsProps) => {
 							{sizeOption !== '' ? `${sizesFiltered[0].sizes.filter((shoeSize: ProductSizes) => shoeSize.size === sizeOption)[0].amount}` : ''} In stock
 						</p>
 					</div>
-					<Toast.Root className={'bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_\'title_action\'_\'description_action\'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut'}
+					<ToastRoot className={'bg-white rounded-md shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] p-[15px] grid [grid-template-areas:_\'title_action\'_\'description_action\'] grid-cols-[auto_max-content] gap-x-[15px] items-center data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out] data-[swipe=end]:animate-swipeOut'}
 						open={open} onOpenChange={setOpen}>
-						<Toast.Title className={'[grid-area:_title] mb-[30px] font-medium text-slate12 text-[15px]'}>
-							<div className='flex flex-row justify-between leading-8'>
+						<Title className={'[grid-area:_title] mb-[30px] font-medium text-slate12 text-[15px]'}>
+							<div className='flex flex-row justify-between leading-8 max-w-[260px]'>
 								<p className='ml-[-8px]'>Added to Cart:</p>
 								<div className='cursor-pointer border-solid border-2 border-black rounded max-h-8 px-2 bg-indigo-500 text-white hover:bg-[#4f46e5]' onClick={() => router.push('/cart')}>To Cart</div>
 							</div>
-						</Toast.Title>
-						<Toast.Description asChild>
-							<div className='[grid-area:_description] mt-5 flex flex-row'>
-								{/* <p className={'[grid-area:_description] mt-5 text-slate11 text-[11px] leading-[1.3] overflow-hidden whitespace-nowrap text-ellipsis'}> */}
-								<img className='max-h-[32px] max-w-[32px] ml-[-40px]' src={productDtl.colors.filter((indColor: Colors) => indColor.color === colorQuery)[0].images[0]} alt='Image of product'/>
-								<p className={'text-slate11 text-[11px] pt-2 leading-[1.3] overflow-hidden whitespace-nowrap text-ellipsis'}>
+						</Title>
+						<Description asChild>
+							<div className='[grid-area:_description] mt-6 flex flex-row gap-2'>
+								<img className='max-h-[32px] max-w-[32px] ml-[-50px]' src={productDtl.colors.filter((indColor: Colors) => indColor.color === colorQuery)[0].images[0]} alt='Image of product'/>
+								<p className={'text-slate11 text-[11px] pt-2 leading-[1.3] overflow-hidden whitespace-nowrap text-ellipsis max-w-[260px]'}>
 									{productDtl.name} - {colorQuery as string} - {sizeOption}
 								</p>
 							</div>
-						</Toast.Description>
-					</Toast.Root>
-					<Toast.Viewport className={'[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none'} />
-				</Toast.Provider>
+						</Description>
+					</ToastRoot>
+					<Viewport className={'[--viewport-padding:_25px] fixed bottom-0 right-0 flex flex-col p-[var(--viewport-padding)] gap-[10px] w-[390px] max-w-[100vw] m-0 list-none z-[2147483647] outline-none'} />
+				</ToastProvider>
 
 			</div>
 		</>
